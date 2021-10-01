ee489aa89066dbb6c5fcb2dc75f3b3c5bcca087e
Gustavo Martins <gm357191@gmail.com>
2021-10-01 14:20:44 -0300

    login (1/3)

---
+++ src/pages/SignIn/index.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { PageArea } from './styled';
+
+import { PageContainer, PageTitle } from '../../components/MainComponents';
+
+const Page = () => {
+    return (
+        <PageContainer>
+            <PageTitle></PageTitle>
+            <PageArea>
+                <form>
+                    <label className="area">
+                        <div className="area--title">E-mail</div>
+                        <div className="area--input">
+                            <input type="email" />
+                        </div>
+                    </label>
+                    <label className="area">
+                        <div className="area--title">Senha</div>
+                        <div className="area--input">
+                            <input type="password" />
+                        </div>
+                    </label>
+                    <label className="area">
+                        <div className="area--title">Lembrar senha</div>
+                        <div className="area--input">
+                            <input type="checkbox" />
+                        </div>
+                    </label>
+                </form>
+            </PageArea>
+        </PageContainer>
+    );
+}
+
+export default Page;
---
+++ src/reducers/userReducer.js
@@ -0,0 +1,12 @@
+const initialScale = {
+    email: '',
+}
+
+export default (state = initialScale, action) => {
+
+    if(action.type == 'SET_EMAIL') {
+        return{...state, email: action.payload.email };
+    }
+
+    return state;
+}
