13ceb9a599ba9d853e792ea96908722cce12fc9a
Gustavo Martins <gm357191@gmail.com>
2021-10-02 00:34:03 -0300

    login (1/3)

--- src/pages/SignIn/styled.js
+++ src/pages/SignIn/styled.js
@@ -2,4 +2,10 @@ import styled from 'styled-components';
 
 export const PageArea = styled.div`
     
+form {
+    background-color: #fff;
+    border-radius: 3px;
+    padding: 10px;
+    box-shadow: 0px 0px 3px #999;
+}
 `;
