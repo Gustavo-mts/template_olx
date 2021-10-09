dd7bd1693e3d62d34baa69fa379f59b9f4213356
Gustavo Martins <gm357191@gmail.com>
2021-10-09 15:48:42 -0300

    adPage - fakeLoading (2/2)

--- src/pages/AdPage/index.js
+++ src/pages/AdPage/index.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import { PageArea } from './styled';
+import { PageArea, Fake } from './styled';
 import { useParams } from 'react-router-dom';
 import useApi from '../../helpers/OlxApi';
 
@@ -10,7 +10,7 @@ const Page = () => {
     const api = useApi();
     const { id } = useParams();
 
-    const [loading, setLoading] = useState([]);
+    const [loading, setLoading] = useState(true);
     const [adInfo, setAdInfo] = useState([]);
 
     return (
@@ -19,20 +19,25 @@ const Page = () => {
                 <div className="leftSide">
                     <div className="box">
                         <div className="adImage">
-                            ...
+                             {loading && <Fake height={300} />}
                         </div>
                         <div className="adInfo">
                             <div className="adName">
-                                ...
+                                {loading && <Fake height={20} />}
                             </div>
                             <div className="adDescription">
-                                ...
+                                 {loading && <Fake height={100}/>}
                             </div>
                         </div>
                     </div>
                 </div>
                 <div className="rightSide">
-                    ...
+                    <div className="box box--padding">
+                        {loading && <Fake height={20}/>}
+                    </div>
+                    <div className="box box--padding">
+                        {loading && <Fake height={50}/>}
+                    </div>   
                 </div>
             </PageArea>
         </PageContainer>
