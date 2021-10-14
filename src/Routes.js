efa4312edaf454a96af9178fa2bc1f5a2ccfba80
Gustavo Martins <gm357191@gmail.com>
2021-10-14 09:33:37 -0300

    private route

--- src/Routes.js
+++ src/Routes.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
+
+import RouteHandler from './components/RouteHandler';
 
 import Home from './pages/Home';
 import About from './pages/About';
@@ -11,24 +13,24 @@ import AdPage from './pages/AdPage';
 export default () => {
     return (
         <Switch>
-            <Route exact path="/">
+            <RouteHandler exact path="/">
                 <Home/>
-            </Route>
-            <Route exact path="/About">
+            </RouteHandler>
+            <RouteHandler exact path="/About">
                 <About/>
-            </Route>
-            <Route exact path="/Signin">
+            </RouteHandler>
+            <RouteHandler exact path="/Signin">
                 <SignIn/>
-            </Route>
-            <Route exact path="/Signup">
+            </RouteHandler>
+            <RouteHandler exact path="/Signup">
                 <SignUp/>
-            </Route>
-            <Route exact path="/ad/:id">
+            </RouteHandler>
+            <RouteHandler exact path="/ad/:id">
                 <AdPage/>
-            </Route>
-            <Route>
+            </RouteHandler>
+            <RouteHandler>
                 <NotFound/>
-            </Route>
+            </RouteHandler>
         </Switch>
     );
 }
