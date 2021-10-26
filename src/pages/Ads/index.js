66f420f08831b54d09a9ea2443a845b402db71d2
Gustavo Martins <gm357191@gmail.com>
2021-10-25 22:58:14 -0300

    ads -- estrutura (1/3)

---
+++ src/pages/Ads/index.js
@@ -0,0 +1,58 @@
+import React, {useState, useEffect} from 'react';
+import {Link} from 'react-router-dom';
+import { PageArea } from './styled';
+import useApi from '../../helpers/OlxApi';
+
+import { PageContainer } from '../../components/MainComponents';
+import AdItem from '../../components/partials/AdItem';
+
+
+const Page = () => {
+    const api = useApi();
+
+    const [stateList, setStateList] = useState([]);
+    const [categories, setCategories] = useState([]);
+    const [adsList, setAdsList] = useState([]);
+
+    useEffect(()=>{
+        const getStates = async() =>{
+            const slist = await api.getStates();
+            setStateList(slist);
+        }
+        getStates();
+    }, []);
+
+    useEffect(()=>{
+        const getCategories = async() =>{
+            const cats = await api.getCategories();
+            setCategories(cats);
+        }
+        getCategories();
+    }, []);
+
+    useEffect(()=>{
+        const getRecentAds = async() =>{
+            const json = await api.getAds({
+                sort: 'desc',
+                limit: 8
+            });
+            setAdsList(json.ads);
+        }
+        getRecentAds();
+    }, []);
+
+    return (
+        <PageContainer>
+            <PageArea>
+                <div className="leftSide">
+                    ...
+                </div>
+                <div className="rightSide">
+                    ....
+                </div>
+            </PageArea>
+        </PageContainer>
+    );
+}
+
+export default Page;
