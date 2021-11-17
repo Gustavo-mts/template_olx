874d3a7fcdfabf855f7490801a6924d6255c4776
Gustavo Martins <gm357191@gmail.com>
2021-11-17 14:03:31 -0300

    myAds

--- src/pages/MyAds/index.js
+++ src/pages/MyAds/index.js
@@ -1,17 +1,94 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { PageArea } from './styled';
+import { Slide } from 'react-slideshow-image';
+import 'react-slideshow-image/dist/styles.css';
+import { useParams } from 'react-router-dom';
+import useApi from '../../helpers/OlxApi';
 
 import {PageContainer} from '../../components/MainComponents';
 
 
 const Page = () => {
-    
+    const api = useApi();
+    const { id } = useParams();
+
+    const [adInfo, setAdInfo] = useState([]);
+ 
+    useEffect(()=>{
+        const getAdInfo = async (id) => {
+            const json = await api.getAd(id, true);
+            return json;
+        }
+        getAdInfo(id);
+    },[]);
+
+    const formatDate= (date) => {
+        let cDate = new Date(date);
+
+        let months = ['janeiro','fevereiro','março','abril','maio','junho','julho','agosto','setembro','outubro','novembro','dezembro'];
+        let cDay = cDate.getDate();
+        let cMonth = cDate.getMonth();
+        let cYear = cDate.getFullYear();
+
+        return `${cDay} de ${months[cMonth]} de ${cYear}`; 
+    }
 
     return (
         <>
            <PageArea>
                <PageContainer style={{backgroundColor: '#fff'}}>
-                   ...
+               <div className="leftSide">
+                    <div className="box">
+                        <div className="adImage">
+                            {adInfo.images &&
+                                <Slide>
+                                    {adInfo.images.map((img, k) =>
+                                        <div key={k} className="each-slide">
+                                            <img src={img} alt="" />
+                                        </div>
+                                    )}
+                                </Slide>
+                            }
+                        </div>
+                        <div className="adInfo">
+                            <div className="adName">
+                                {adInfo.title &&
+                                    <h2>{adInfo.title}</h2>
+                                }
+                                {adInfo.dateCreated &&
+                                    <small>criado em {formatDate(adInfo.dateCreated)}</small>
+                                }
+                            </div>
+                            <div className="adDescription">
+                                 {adInfo.description}
+                                 <hr/>
+                                 {adInfo.views &&
+                                    <small>Visualizações: {adInfo.views}</small>
+                                 }
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="rightSide">
+                    <div className="box box--padding">
+                        {adInfo.priceNegotiable &&
+                            'Preço Negociável'
+                        }
+                        {!adInfo.priceNegotiable && adInfo &&
+                            <div className="price">Preço: <span>${adInfo.price}</span></div>
+                        }
+                    </div>
+                        {adInfo.userInfo &&
+                            <>
+                                <a href={`mailto:${adInfo.userInfo.email}`} target="_blanck" className="contactSellerLink">Fale conosco</a>
+                                <div className="createdBy box box--padding">
+                                    <strong>{adInfo.userInfo.name}</strong>
+                                    <small>Email: {adInfo.userInfo.email}</small>
+                                    <small>Estado: {adInfo.stateName}</small>
+                                </div>
+                            </>
+                        }
+                </div>
                </PageContainer>
            </PageArea>
         </>
--- src/pages/MyAds/styled.js
+++ src/pages/MyAds/styled.js
@@ -9,4 +9,101 @@ z-index: 99;
 position: absolute;
 margin-top: -715px;
 margin-left: -97px;
+
+.box {
+    background-color:#fff;
+    border-radius:5px;
+    box-shadow:0px 0px 4px #999;
+    margin-bottom:20px;
+}
+. box--padding {
+    padding:10px;
+}
+
+.leftSide {
+    flex: 1;
+    margin-right:20px;
+
+    .box {
+        display: flex;
+    }
+
+    .adImage {
+        width: 320px;
+        height: 320px;
+        margin-right: 20px;
+
+        .each-slide img {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            background-size: cover;
+            height:320px;
+        }
+    }
+
+    .adInfo {
+        padding:10px; 
+
+        .adName {
+            margin-bottom:20px;
+
+            h2 {
+                margin: 0;
+                margin-left: 20px
+            }
+
+            small {
+                color: #999;
+            }
+        }
+
+        .adDescription {
+            small {
+                color: #999;
+            }
+        }
+    }
+}
+.rightSide {
+    width: 250px;
+
+    .price {
+        margin-left: 5px;
+    
+        span {
+            color: #0000ff;
+            display: block;
+            font-size: 27px;
+            font-weight: bold;
+        }
+    }
+
+    .contactSellerLink {
+        background-color: #0000ff;
+        color: #fff;
+        height: 30px;
+        border-radius:5px;
+        box-shadow:0px 0px 4px #999;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        text-decoration: none;
+        margin-bottom: 20px;
+    }
+
+    .createdBy
+
+        strong {
+            margin-left: 5px;
+        }
+        
+        small {
+            display: block;
+            color: #999;
+            margin-top: 10px;
+            margin-left: 5px;
+        }
+    }
+}
 `;
